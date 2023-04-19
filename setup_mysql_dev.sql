50e1d3865f7125862b4887fe47d69327208319ca
James Aworo <james001.awo@gmail.com>
2023-04-19 09:49:19 -0700

    update database setup script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,6 @@
--- prepares a MySQL server for the project
--- GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
-
+-- Create dev database and user
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
-USE hbnb_dev_db;
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-USE performance_schema;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,6 @@
--- prepares a MySQL server for the project
--- GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
-
+-- Create test database and user
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
-USE hbnb_test_db;
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-USE performance_schema;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
