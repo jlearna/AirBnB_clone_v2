01df7b6b8f7901c2bd86357931663b760875998b
James Aworo <james001.awo@gmail.com>
2023-05-19 04:25:53 -0700

    fix templates

--- models/city.py
+++ models/city.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
-"""This is the city class"""
+""" holds class City"""
 import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
 
 class City(BaseModel, Base):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
-    """
-    # initialize class for file/db storage type
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place', cascade='all, delete', backref='cities')
+    """Representation of city """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'cities'
+        name = Column(String(128),
+                      nullable=False)
+        state_id = Column(String(60),
+                          ForeignKey('states.id'),
+                          nullable=False)
+        places = relationship("Place",
+                              backref="cities",
+                              cascade="all, delete-orphan")
+    else:
+        name = ""
+        state_id = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
--- models/user.py
+++ models/user.py
@@ -1,24 +1,47 @@
 #!/usr/bin/python3
-"""This is the user class"""
+""" holds class User"""
+import hashlib
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from os import getenv
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = 'users'
+    """Representation of a user """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128),
+                       nullable=False)
+        _password = Column('password',
+                           String(128),
+                           nullable=False)
+        first_name = Column(String(128),
+                            nullable=True)
+        last_name = Column(String(128),
+                           nullable=True)
+        places = relationship("Place",
+                              backref="user",
+                              cascade="all, delete-orphan")
+        reviews = relationship("Review",
+                               backref="user",
+                               cascade="all, delete-orphan")
+    else:
+        email = ""
+        _password = ""
+        first_name = ""
+        last_name = ""
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
 
-    reviews = relationship('Review', cascade='all, delete', backref='user')
-    places = relationship('Place', cascade='all, delete', backref='user')
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, pwd):
+        """hashing password values"""
+        self._password = pwd
