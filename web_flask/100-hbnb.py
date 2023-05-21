ca14fc06c3dd809958df3d29123dd119cd7a9fcc
James Aworo <james001.awo@gmail.com>
2023-05-21 11:10:53 -0700

    fix template

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
-from models import storage
-from flask import Flask
-from flask import render_template
+"""
+Starts a flask application
+"""
 
+
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
-def hbnb_filters():
-    """Displays the main HBnB filters HTML page."""
-    states = storage.all("State")
-    amenities = storage.all("Amenity")
-    return render_template("10-hbnb_filters.html",
-                           states=states, amenities=amenities)
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ return all data in the db  """
+    states = storage.all("State").values()
+    amenities = storage.all("Amenity").values()
+    places = storage.all("Place").values()
+    print(places)
+    return render_template('100-hbnb.html',
+                           states=states,
+                           places=places,
+                           amenities=amenities)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_data(self):
+    """ closes the storage on teardown"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
